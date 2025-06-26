ae68b741efa58d39364d177367b383ed1fb36ed0
SteppedTax <vadimsilagin26@gmail.com>
2025-06-26 15:14:00 +0300

    Fully refactor BurnUnderSunlight

--- src/main/java/net/steppedtax/racesrewrite/races/undead/BurnUnderSunlight.java
+++ src/main/java/net/steppedtax/racesrewrite/races/undead/BurnUnderSunlight.java
@@ -1,25 +1,21 @@
 package net.steppedtax.racesrewrite.races.undead;
 
-import org.bukkit.World;
-import org.bukkit.entity.Entity;
+import net.steppedtax.racesrewrite.RacesRewrite;
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
 
-public abstract class BurnUnderSunlight implements World {
+import java.util.Objects;
+
+public class BurnUnderSunlight extends BukkitRunnable {
+    private final RacesRewrite plugin;
+    public BurnUnderSunlight(RacesRewrite plugin) {
+        this.plugin = plugin;
+    }
 
     @Override
-    public long getTime() { // infinite recursion, rewrite this
-        long gameTime = getTime();
-        if ((gameTime >= 12542L) && (gameTime <= 23460L) || !(isClearWeather())) { // if night or not clear weather:
-            return gameTime;
-        }
-        else if (isClearWeather()) { // if day and clear weather:
-            // idfk ive been working on this for five hours straight and my dopamine receptors are fucking starving
-            // ive went through all nine stages of grief five times in a row
-            // i don't understand why i cant implement both the world and the entity WHY IS IT RED
-            // i had to cut out a lot of functionality from this plugin because it was just not fucking working
-            // i shouldve better been working on listeners than on this shit
-            // fuck this shit
-            return 0;
-        }
-        return gameTime;
+    public void run() {
+        long dayTime = Objects.requireNonNull(Bukkit.getServer().getWorld("world")).getTime();
+        plugin.getLogger().info("Current time: " + dayTime);
     }
 }
+// (gameTime >= 12542L) && (gameTime <= 23460L)
