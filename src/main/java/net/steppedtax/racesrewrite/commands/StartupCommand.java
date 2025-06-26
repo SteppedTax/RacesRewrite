27c5897e3d77b6d856700fa81f13b353ee187b5d
SteppedTax <vadimsilagin26@gmail.com>
2025-06-26 12:34:18 +0300

    Added StartupCommand

---
+++ src/main/java/net/steppedtax/racesrewrite/commands/StartupCommand.java
@@ -0,0 +1,36 @@
+package net.steppedtax.racesrewrite.commands;
+
+import net.steppedtax.racesrewrite.RacesRewrite;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+
+public class StartupCommand implements CommandExecutor {
+    private final RacesRewrite plugin;
+    public StartupCommand(RacesRewrite plugin) {
+        this.plugin = plugin;
+    }
+    // love doing random stuff when I'm bored
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        String formattedArgs = Arrays.toString(args)
+                .replace(",", "")
+                .replace("[", "")
+                .replace("]", "")
+                .trim();
+        if (args.length >= 1) {
+            this.plugin.getConfig().set("startup-message", formattedArgs);
+            sender.sendMessage("Startup message changed to: " + formattedArgs);
+        }
+        else {
+            String defaultStartupMessage = "The plugin has been loaded!";
+            this.plugin.getConfig().set("startup-message", defaultStartupMessage);
+            sender.sendMessage("Startup message reverted to default.");
+        }
+        this.plugin.saveConfig();
+        return true;
+    }
+}
