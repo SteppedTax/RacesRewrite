9ac5b83b5ab0be2e871e8f8d65bcd9a97fa3dc50
SteppedTax <vadimsilagin26@gmail.com>
2025-07-03 20:20:13 +0300

    Races Rewrite 0.5.0: First release!
    Changes:
    - The Undead race is now fully implemented!
    - Now Undead players have a chance to defend themselves:
    - If they wear anything on their head it will protect them from the sun, but will break over time.
    - Very minor changes
    
    PS: The next update will start development on the Plant race, as the Undead are now fully complete.

--- src/main/java/net/steppedtax/racesrewrite/races/undead/listeners/UndeadMonster.java
+++ src/main/java/net/steppedtax/racesrewrite/races/undead/listeners/UndeadMonster.java
@@ -2,5 +2,6 @@ package net.steppedtax.racesrewrite.races.undead.listeners;
 
 import org.bukkit.entity.Monster;
 
+@Deprecated
 public interface UndeadMonster extends Monster {} // had to cut this all out
 // CaveSpider, Creeper, Drowned, Husk, Skeleton, Spider, Stray, Witch, Zombie, ZombieVillager
--- src/main/java/net/steppedtax/racesrewrite/races/undead/tasks/BurnUnderSunlight.java
+++ src/main/java/net/steppedtax/racesrewrite/races/undead/tasks/BurnUnderSunlight.java
@@ -3,33 +3,68 @@ package net.steppedtax.racesrewrite.races.undead.tasks;
 import net.steppedtax.racesrewrite.RacesRewrite;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Random;
+import java.util.Set;
 
 public class BurnUnderSunlight extends BukkitRunnable {
     private final RacesRewrite plugin;
     public BurnUnderSunlight(RacesRewrite plugin) {
         this.plugin = plugin;
     }
+    Random rand = new Random();
 
     @Override
     public void run() {
         var onlinePlayers = Bukkit.getOnlinePlayers();
         for (Player player : onlinePlayers) {
-            if (playerIsValid(player) && isDay(player) && isUnderSky(player)) {
-                player.setFireTicks(100);
+            if (playerIsValid(player) && isClearDay(player) && isUnderSky(player)) {
+                ItemStack headSlot = player.getInventory().getHelmet();
+
+                if (isHeadSlotEmpty(headSlot)) {
+                    player.setFireTicks(60);
+                }
+                if (hasHelmetEquipped(headSlot)) {
+                    short durability = getDurability(headSlot);
+                    short finalDurability = (short) (durability + durabilityAddend(headSlot));
+
+                    setDurability(headSlot, finalDurability);
+                }
+                else if (!hasHelmetEquipped(headSlot)) {
+                    if (rand.nextInt(128) < 8) {
+                        player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 10, 1);
+                        player.getInventory().setHelmet(null);
+                    }
+                }
             }
         }
     }
 
-    private boolean isDay(Player player) {
+    private boolean isClearDay(Player player) {
         long dayTime = player.getWorld().getTime();
+        boolean clearWeather = player.getWorld().isClearWeather();
         // Bukkit.getLogger().info("Current time: " + dayTime);
-        return (dayTime <= 12542L) || (dayTime >= 23460L);
+        return (dayTime <= 12542L) || (dayTime >= 23460L) && clearWeather;
     }
 
     private boolean isUnderSky(Player player) {
         return player.getLocation().getBlock().getLightFromSky() == 15;
+    }
+
+    private boolean isHeadSlotEmpty(ItemStack headSlot) {
+        return headSlot != null && headSlot.getType() == Material.AIR;
+    }
+
+    private boolean hasHelmetEquipped(ItemStack headSlot) {
+        return headSlot != null && HELMET.contains(headSlot.getType());
     }
 
     private boolean playerIsValid(Player player) {
@@ -39,5 +74,53 @@ public class BurnUnderSunlight extends BukkitRunnable {
         }
         return false;
     }
+
+    public short getDurability(ItemStack headSlot) {
+        ItemMeta meta = headSlot.getItemMeta();
+        return (meta == null) ? 0 : (short) ((Damageable) meta).getDamage();
+        // this is where I learned about ternary operators...
+    }
+
+    public void setDurability(ItemStack headSlot, short durability) {
+        ItemMeta meta = headSlot.getItemMeta();
+        if (meta != null) {
+            ((Damageable) meta).setDamage(durability);
+            headSlot.setItemMeta(meta);
+        }
+        // shoutout to craftbukkit source code
+    }
+
+    private short durabilityAddend(ItemStack headSlot) {
+        int chance = 10;
+        if (headSlot.containsEnchantment(Enchantment.UNBREAKING)) {
+            int level = headSlot.getEnchantmentLevel(Enchantment.UNBREAKING);
+            switch (level) {
+                case 1:
+                    chance = 25;
+                    break;
+                case 2:
+                    chance = 36;
+                    break;
+                case 3:
+                    chance = 43;
+                    break;
+            }
+        }
+        if (rand.nextInt(100) >= chance) {
+            return 1;
+        }
+        return 0;
+    }
+
+    private final Set<Material> HELMET = Set.of(
+            Material.LEATHER_HELMET,
+            Material.GOLDEN_HELMET,
+            Material.CHAINMAIL_HELMET,
+            // Material.COPPER_HELMET,
+            Material.IRON_HELMET,
+            Material.DIAMOND_HELMET,
+            Material.NETHERITE_HELMET,
+            Material.TURTLE_HELMET // special
+    );
 }
 // (gameTime >= 12542L) && (gameTime <= 23460L)
