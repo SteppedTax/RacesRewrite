067a4e2df3dda7c136a156331ae70b3a7f6c2e66
SteppedTax <vadimsilagin26@gmail.com>
2025-07-08 12:48:26 +0300

    Plants now regenerate hunger on grass, moss and leaves

--- src/main/java/net/steppedtax/racesrewrite/RacesRewrite.java
+++ src/main/java/net/steppedtax/racesrewrite/RacesRewrite.java
@@ -4,6 +4,7 @@ import net.steppedtax.racesrewrite.commands.RaceCommand;
 import net.steppedtax.racesrewrite.commands.RaceCommandAutocomplete;
 import net.steppedtax.racesrewrite.commands.StartupCommand;
 import net.steppedtax.racesrewrite.races.plants.listeners.DoubleFireDamage;
+import net.steppedtax.racesrewrite.races.plants.tasks.PlantsRegenHunger;
 import net.steppedtax.racesrewrite.races.undead.listeners.HostileGolems;
 import net.steppedtax.racesrewrite.races.undead.listeners.NeutralHostileMobs;
 import net.steppedtax.racesrewrite.races.undead.listeners.ToxicPlantFood;
@@ -39,6 +40,7 @@ public final class RacesRewrite extends JavaPlugin {
         BukkitTask AutosaveTask = new AutosaveTask(this).runTaskTimer(this, 600L, 600L);
         BukkitTask BurnUnderSunlight = new BurnUnderSunlight(this).runTaskTimer(this, 60L, 60L);
         BukkitTask RegenInDarkness = new RegenInDarkness().runTaskTimer(this, 40L, 40L);
+        BukkitTask PlantsRegenHunger = new PlantsRegenHunger().runTaskTimer(this, 32L, 32L);
         // Other stuff
         Objects.requireNonNull(this.getCommand("race")).setExecutor(new RaceCommand(this));
         Objects.requireNonNull(this.getCommand("race")).setTabCompleter(new RaceCommandAutocomplete(this));
---
+++ src/main/java/net/steppedtax/racesrewrite/races/plants/tasks/PlantsRegenHunger.java
@@ -0,0 +1,47 @@
+package net.steppedtax.racesrewrite.races.plants.tasks;
+
+import net.steppedtax.racesrewrite.RacesRewrite;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Set;
+
+public class PlantsRegenHunger extends BukkitRunnable {
+
+    @Override
+    public void run() {
+        var onlinePlayers = Bukkit.getOnlinePlayers();
+        for (Player player : onlinePlayers) {
+            if (playerIsPlant(player) && plantBlockBelow(player) && player.getLocation().getBlock().getLightLevel() == 15) {
+                if (player.getFoodLevel() < 20) {
+                    // TODO: We need more feedback for hunger regeneration
+                    player.setFoodLevel(player.getFoodLevel() + 1);
+                    player.sendMessage(String.valueOf(player.getFoodLevel()));
+                }
+            }
+        }
+    }
+
+    private boolean plantBlockBelow(Player player) {
+        Material block = player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType();
+        return PLANT_BLOCK.contains(block) || block.toString().contains("LEAVES");
+    }
+
+    private boolean playerIsPlant(Player player) {
+        String playerID = player.getUniqueId().toString();
+        if (RacesRewrite.plantPlayers.contains(playerID)) {
+            return player.getGameMode() == GameMode.SURVIVAL; // return true
+        }
+        return false;
+    }
+
+    private final Set<Material> PLANT_BLOCK = Set.of(
+            Material.GRASS_BLOCK,
+            Material.MOSS_BLOCK
+            // add more later
+    );
+}
